4ddb6334785ee13a7c38e01c3d5c0c24afd16e58
mitul_manish <mitul.manish@gmail.com>
2019-03-01 11:44:51 +1100

    Separating the code into extension

--- Example/Slidoo/BaseViewController.swift
+++ Example/Slidoo/BaseViewController.swift
@@ -11,7 +11,9 @@ class BaseViewController: UIViewController {
         screenEdgeGesture.edges = view.isRTL ? .right : .left
         view.addGestureRecognizer(screenEdgeGesture)
     }
-
+    
+    // MARK: - Actions
+    
     @objc func didPan(panRecognizer: UIPanGestureRecognizer) {
         switch panRecognizer.state {
         case .began:
@@ -22,24 +24,28 @@ class BaseViewController: UIViewController {
             break
         }
     }
+    
+    @IBAction func buttonTouched(_ sender: UIBarButtonItem) {
+        presentMenu(supportAnimation: true)
+    }
+}
 
+// MARK: - Helpers
+
+extension BaseViewController {
     private func forwardPanGesture(_ panRecognizer: UIPanGestureRecognizer) {
         if let presentedVC = presentedViewController as? DrawerViewController, let presentationController = presentedVC.presentationController as? NavigationDrawerSwipeController {
             presentationController.didPan(panRecognizer: panRecognizer, screenGestureEnabled: true)
         }
     }
-
+    
     private func presentMenu(supportAnimation: Bool) {
         guard let presentedVC = getDrawerViewController() else { return }
         presentedViewControllerTransitionAnimator = nil
         presentedViewControllerTransitionAnimator = NavigationDrawerTransitionDelegate(supportAnimation: supportAnimation)
-        presentedVC.transitioningDelegate = self.presentedViewControllerTransitionAnimator
+        presentedVC.transitioningDelegate = presentedViewControllerTransitionAnimator
         presentedVC.modalPresentationStyle = .custom
         present(presentedVC, animated: true)
-    }
-
-    @IBAction func buttonTouched(_ sender: UIBarButtonItem) {
-        presentMenu(supportAnimation: true)
     }
     
     private func getDrawerViewController() -> UIViewController? {
@@ -49,4 +55,3 @@ class BaseViewController: UIViewController {
         return menuVC
     }
 }
-
